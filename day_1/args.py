3d40603b6590705ec7aa110a582b9845e9e77637
Thomas Ménès <64870819+OBud75@users.noreply.github.com>
2025-05-13 06:39:55 +0200

    Update args.py

--- day_1/args.py
+++ day_1/args.py
@@ -1,2 +1,8 @@
 def toto(pos_only, /, standard, *, kwarg_only):
   ...
+
+
+"""
+Tout ce qui est avant le / doit être passé comme un argument positionnel.
+Tout ce qui est après le * doit être passé sous forme d'argument nommé.
+"""
